ec748f58c4de47a892ab92f1c45f7b2b8f21bd56
sergiomarchena16 <mar16387@uvg.edu.gt>
2018-02-23 19:40:42 -0600

    some little changes

--- beta1.py
+++ beta1.py
@@ -1,48 +1,51 @@
+# HOJA 5 - SIMULACIONES
+
 import simpy
 import random
 
 
+# sergio marchena 16387
+
+
 
 #solo necesitamos definir ready. Porque en ready es donde pasa todo. 
-def ready(nombre, env, instrucciones_totales,instrucciones_restantes):
+def ready(nombre, env, instruc,resInstruc):
 
     global memoria # :( mala practica, pero ni modo
 
-    memoria=memoria+instrucciones_totales
-    print('El proceso #%d esta listo para ser procesado con %d instrucciones' % (nombre, instrucciones_restantes))#ready
+    memoria=memoria+instruc
+    print('El proceso #%d esta listo para ser procesado con %d instrucciones' % (nombre, resInstruc))#ready
     yield env.timeout(5)
     #RUNNING
-    print memoria
     with cpu.request() as hola:
         yield hola
         yield env.timeout(10)
-        if(instrucciones_restantes>3):
+        if(resInstruc>3):
             print('El proceso #%d esta se esta procesando en tiempo %d' % (nombre, env.now))
             flag=random.randint(0,1)
-            print memoria
             #WAITING
             if(flag==1): #random entre 1 y 2
                 print('El proceso #%d esta esperando entrada/salida en tiempo %d' % (nombre, env.now))
                 yield env.timeout(20)
-                ready(nombre, env, instrucciones_totales,instrucciones_restantes - 3)
-                print memoria
+                ready(nombre, env, instruc,resInstruc - 3)
             else:
-                ready(nombre, env, instrucciones_totales,instrucciones_restantes - 3)
+                ready(nombre, env, instruc,resInstruc - 3)
         else:
             #TERMINATED
             print('El proceso #%d finalizo en tiempo %d'%(nombre,env.now))
-            print memoria
-            memoria=memoria-instrucciones_totales
+            memoria=memoria-instruc
 
 
-# ------------------------------- ENVIRONMENT --------------------------- #
+# ------------------------------- ENVIRONMENT and VARIABLES --------------------------- #
 
+# memoria, capacity, instrucciones, etc.
 
 env = simpy.Environment()
-cpu= simpy.Resource(env,capacity=1)
-memoria=0 #LA MEMORIA ES 0 Y LA ACOMODAMOS A LOS PROCESOS QUE USEN
+cpu = simpy.Resource(env,capacity=1)
+memoria = 0 #LA MEMORIA ES 0 Y LA ACOMODAMOS A LOS PROCESOS QUE USEN
+
 for i in range(5):
     instrucciones = random.randint(1,10)
     print ('El proceso %d# se ha creado en %d con %d instrucciones' % (i, env.now, instrucciones))
     env.process(ready(i,env,instrucciones,instrucciones))
-env.run(until=9999999)
+env.run(until=100000)
